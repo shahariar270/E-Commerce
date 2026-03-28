3c5d034cb0498923114dbbcb9e6ae32b52d5583f
shahariar270 <mdshahariar102@gmail.com>
2026-03-28 11:09:47 +0600

    handle routes

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
+const router = require('./router');
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 const port = process.env.PORT || 10000;
 
