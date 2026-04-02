6efb214be791f8dcc924a831c83f85b7b4bdef77
shahariar270 <mdshahariar102@gmail.com>
2026-04-02 22:14:03 +0600

    update origin

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ const { default: mongoose } = require('mongoose');
 const router = require('./router');
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
