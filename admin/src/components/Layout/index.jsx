635f30627e6ed86211dfd919534dbf244ed52b42
shahariar270 <mdshahariar102@gmail.com>
2026-04-17 22:13:48 +0600

    some basic layout page added

--- admin/src/components/Layout/index.jsx
+++ admin/src/components/Layout/index.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar";
-
-
+import { Topbar } from "@Component/Topbar";
+// import "./styles/layout.scss";
+import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
-
+  const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
-    <div className={'st-layout'}>
-      <Sidebar />
+    <div className={`st-layout ${sidebarOpen ? "st-layout--sidebar-open" : "st-layout--sidebar-collapsed"}`}>
+      <Sidebar isOpen={sidebarOpen} onToggle={() => setSidebarOpen(prev => !prev)} />
+
+      <div className="st-layout__body">
+        <Topbar onMenuToggle={() => setSidebarOpen(prev => !prev)} />
+
+        <main className="st-layout__main">
+          <div className="st-layout__content">
+            <Outlet />
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
