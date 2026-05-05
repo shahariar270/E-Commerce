2022a61d925ad765fc19e502017574f1edc32324
shahariar270 <mdshahariar102@gmail.com>
2026-05-05 06:29:05 +0600

    dashboard query done

--- server/router/dashboard/index.js
+++ server/router/dashboard/index.js
@@ -5,8 +5,8 @@ const dashboard = require('../../controls/dashboard');
 
 router.get(
     '/dashboard/cards',
-    // auth_middleware.verify_token,
-    // auth_middleware.verify_role('admin'),
+    auth_middleware.verify_token,
+    auth_middleware.verify_role('admin'),
     dashboard.get_card_data
 )
 
