7ffc85b7fd5fe2fd7a1926d2d65ba2c9543277b6
shahariar270 <mdshahariar102@gmail.com>
2026-05-10 21:11:42 +0600

    register router

--- server/router/comment/index.js
+++ server/router/comment/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const auth_middleware = require('../../middlewares/auth_middleware');
-const { comment_create } = require('../../controls/comment');
+const { comment_create, update_comment } = require('../../controls/comment');
 const router = express.Router();
 
 router.post(
     '/comment/:id',
-     auth_middleware.verify_token,
-     comment_create
+    auth_middleware.verify_token,
+    comment_create
 );
+
+router.put(
+    '/comment/:id?comment_id',
+    auth_middleware.verify_token,
+    update_comment
+)
 
 
 module.exports = router;
