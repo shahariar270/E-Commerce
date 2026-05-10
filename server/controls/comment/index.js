39c5854d0dc2306e9e20465bf4388eecc1317e3a
shahariar270 <mdshahariar102@gmail.com>
2026-05-10 06:58:53 +0600

    issue fixed

--- server/controls/comment/index.js
+++ server/controls/comment/index.js
@@ -1,8 +1,26 @@
+const Comment = require("../../model/conmment");
 
 
 class comment_controls {
     async comment_create(req, res) {
         try {
+            const user_id = req?.user?.id;
+            const product_id = req?.params?.id
+            const { content, parent } = req.body;
+
+            const new_comment = await Comment.create({
+                product: product_id,
+                author: user_id,
+                content,
+                parent,
+            })
+
+            return res.status(201).json({
+                success: true,
+                data: new_comment,
+                message: "comment created successfully"
+            })
+
         } catch (error) {
             return res.status(500).json({
                 message: error.message,
