3fc43661f8dd686c5c25407396173aaf898524e4
shahariar270 <mdshahariar102@gmail.com>
2026-05-12 06:38:16 +0600

    respoonse added

--- server/utils/api_response.js
+++ server/utils/api_response.js
@@ -1,11 +1,22 @@
-
-class response {
-    constructor(status, message, data, success = true) {
-        this.status = status;
+class ApiResponse {
+    constructor(statusCode, message, data = null, success = true) {
+        this.statusCode = statusCode;
         this.message = message;
         this.data = data;
         this.success = success;
     }
+
+    static success(res, message = "Success", data = null, statusCode = 200) {
+        return res.status(statusCode).json(
+            new ApiResponse(statusCode, message, data, true)
+        );
+    }
+
+    static error(res, message = "Error", statusCode = 500, data = null) {
+        return res.status(statusCode).json(
+            new ApiResponse(statusCode, message, data, false)
+        );
+    }
 }
 
-module.exports = response;
+module.exports = ApiResponse;
