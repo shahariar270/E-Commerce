680d455b2e463b78aabc9e634e7a2010aea84e98
shahariar270 <mdshahariar102@gmail.com>
2026-07-24 18:33:24 +0600

    Add Google login validation schema

--- server/validation_schema/auth/index.js
+++ server/validation_schema/auth/index.js
@@ -35,4 +35,10 @@ const updateProfileSchema = z.object({
     last_name: z.string().min(2).max(100).optional()
 });
 
-module.exports = { registerSchema, loginSchema, updateProfileSchema };
+const googleLoginSchema = z.object({
+    credential: z.string({
+        required_error: "Google credential is required"
+    }).min(1, "Google credential is required")
+});
+
+module.exports = { registerSchema, loginSchema, updateProfileSchema, googleLoginSchema };
