28fcd31c597f0218500908750c373991aeacd500
shahariar270 <mdshahariar102@gmail.com>
2026-07-24 18:33:31 +0600

    Add POST /auth/google route

--- server/router/auth/index.js
+++ server/router/auth/index.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const {
     register_controller,
     login_controller,
+    google_login_controller,
     update_profile_controller,
     profile_controller
 } = require('../../controls/auth');
@@ -14,6 +15,8 @@ const { upload } = require('../../middlewares/file_handle');
 router.post('/register', register_controller);
 
 router.post('/login', login_controller);
+
+router.post('/google', google_login_controller);
 
 router.post('/update_profile', auth_middleware.verify_token, upload.single('profile_image'), update_profile_controller);
 
